e1dd411688e0b5f2500b3177fb5bf5a7432af582
erdem.io <erdem_arslan_694@hotmail.com>
2020-10-27 23:29:58 +0300

    Start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+    const uri = process.env.conString;
 
 
     const client = new MongoClient(uri);
