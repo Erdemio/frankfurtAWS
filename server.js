65473ce186a30e4732f3bf922c1f9332eb05d678
erdem.io <erdem_arslan_694@hotmail.com>
2020-10-27 23:33:05 +0300

    Deploy again.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = process.env.conString;
-
+    //const uri = process.env.conString;
+    const uri = "mongodb+srv://webUser1:qwer5678.@clusterio.3ywev.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
