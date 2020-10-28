a29cd108d9faf38932e97f0a8dee565c34ba5566
erdem.io <erdem_arslan_694@hotmail.com>
2020-10-28 13:01:41 +0300

    minor changes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 var port = process.env.PORT || 80;
 const router = express.Router();
+var cors = require('cors')
 
 app.use("/", router);
 
@@ -12,7 +13,7 @@ app.listen(port, function() {
   console.log("Server is running on Port: " + port);
 });
 
-app.get('/word/:word', function (req, res) {
+app.get('/word/:word', cors(), function (req, res) {
   let reqWord = req.params['word'];
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
